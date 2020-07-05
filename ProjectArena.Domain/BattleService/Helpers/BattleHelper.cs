cee7267a6a65bce0b8fc705bb722ec9be27464be
Ig101 <nsn1001@mail.ru>
2020-07-05 20:28:43 +0300

    Fix botlearning variables

--- ProjectArena.Domain/BattleService/Helpers/BattleHelper.cs
+++ ProjectArena.Domain/BattleService/Helpers/BattleHelper.cs
@@ -56,7 +56,7 @@ namespace ProjectArena.Domain.BattleService.Helpers
                         Mode = new SceneMode()
                         {
                             Generator = EngineHelper.CreateDuelSceneGenerator(),
-                            VarManager = EngineHelper.CreateVarManager(10, true, 8, 4, 0.05f, 0.05f, 0.03f, 0.4f),
+                            VarManager = EngineHelper.CreateVarManager(10, true, 8, 16, 0.05f, 0.05f, 0.03f, 0.4f),
                             BattleResultProcessor = BattleResultProcessors.ProcessMainDuelBattleResult,
                             MaxPlayers = 2,
                             TimeTillBot = 0,
